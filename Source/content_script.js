0d1fadf0d4ddb687625a3a98b462f2283d434117
Kriztopher Mendoza <kriz@krizinc.com>
2013-09-20 08:16:53 -0700

    Update content_script.js
    Change butt to heiney because it's more appropriate for our team

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bThe Cloud\b/g, "My Heiney");
+	v = v.replace(/\bThe cloud\b/g, "My heiney");
+	v = v.replace(/\bthe Cloud\b/g, "my Heiney");
+	v = v.replace(/\bthe cloud\b/g, "my heiney");
 	
 	textNode.nodeValue = v;
 }
